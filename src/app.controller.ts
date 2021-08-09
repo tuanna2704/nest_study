200b71a35734a350377fd278c8836b08a3569ebd
tuanna2704@gmail.com <tuanna2704@gmail.com>
2021-08-09 13:20:48 +0700

    test controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('test/subtest')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // Go to this page by URL: /test/subTest/foo/subFoo
+  @Get('foo/subFoo')
+  getFoo(): string {
+    return 'This is Foo'
   }
 }
