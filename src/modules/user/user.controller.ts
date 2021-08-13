027272e9977a684631da3162ecf23576ce8398ab
Anh Tuan <tuanna2704@gmail.com>
2021-08-13 10:21:42 +0700

    add new controller with restful methods

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  
+  @Get()
+  index() {
+    return 'index page';
+  }
+
+  @Get('new')
+  new(){
+    return 'new page'
+  }
+
+  @Post('new')
+  create(){
+    return 'create page'
+  }
+
+
+  @Get(':id')
+  show( @Param('id')id: string ) {
+    return {
+      message: 'getOne',
+      id
+    }
+  }
+
+  @Get(':id/edit')
+  edit( @Param('id')id: string ) {
+    return {
+      message: 'edit page',
+      id
+    }
+  }
+
+  @Put(':id')
+  update( @Param('id')id: string ) {
+    return {
+      message: 'update page',
+      id
+    }
+  }
+
+  @Delete(':id')
+  delete( @Param('id')id: string ) {
+    return {
+      message: 'delete page',
+      id
+    }
+  }
+}
