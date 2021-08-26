4e302237d130f1b197f61376e56433bbaa0140df
Anh Tuan <tuanna2704@gmail.com>
2021-08-26 11:37:05 +0700

    [Demo] Add new method to grpc

--- src/modules/grpc-service/grpc-service.controller.ts
+++ src/modules/grpc-service/grpc-service.controller.ts
@@ -4,6 +4,11 @@ import { ClientGrpcProxy, GrpcMethod } from '@nestjs/microservices';
 @Controller('grpc-service')
 export class GrpcServiceController {
   private grpcService;
+  private items = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Doe' },
+  ];
+
   constructor (
     @Inject('GRPC_MICROSERVICE') private grpcClient: ClientGrpcProxy,
   ) {}
@@ -14,18 +19,29 @@ export class GrpcServiceController {
   }   
 
   @Get()
-  accumulate(@Query() { data })  {
+  find(@Query() { data })  {
     console.log(this.grpcService)
     return this.grpcService.findOne({ id: 1 });
   }
 
+  @Get('all')
+  findAll()  {
+    return this.grpcService.findAll();
+  }
+
+  // SERVER
   @GrpcMethod('GrpcDemoController', 'FindOne')
-  findOne(data, metadata, call) {
+  findGRPC(data, metadata, call) {
     const items = [
       { id: 1, name: 'John' },
       { id: 2, name: 'Doe' },
     ];
-    return items.find(({ id }) => id === data.id);
+    return this.items.find(({ id }) => id === data.id);
+  }
+
+  @GrpcMethod('GrpcDemoController', 'FindAll')
+  findAllGRPC() {
+    return this.items
   }
 
 }
